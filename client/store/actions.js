9228c42e2dfb16d29fee39ed1bbbc4bc487fb96a
Coty Condry <ccondry@cisco.com>
2018-02-14 01:19:22 -0600

    fix autologin and parameter passing for chat with aqua template

--- client/store/actions.js
+++ client/store/actions.js
@@ -126,24 +126,109 @@ export const uploadImage = async function ({getters, commit, dispatch}, data) {
   }
 }
 
-export const openChat = function ({getters, commit, dispatch}, data) {
-  try {
-    console.log(`open chat`, data)
-    // data.name
-    // data.email
-    // data.ani
-    // data.message
-    const template = 'kiwi'
-    const entryPointId = '1002'
-    const locale = 'en-US'
-    // https://ece2.cxdemo.net/system/templates/chat/kiwi/chat.html?subActivity=Chat&entryPointId=1006&templateName=kiwi_alto&languageCode=en&countryCode=US&ver=v11
-    let url = `https://ece2.cxdemo.net/system/templates/chat/${template}/chat.html`
-    url += `?subActivity=Chat&entryPointId=${entryPointId}&templateName=${template}&languageCode=${locale.split('-')[0]}&countryCode=${locale.split('-')[1]}&ver=v11`
-    url += `&fieldname1=${encodeURIComponent(data.name)}&fieldname2=${data.email}&fieldname3=${data.ani}&fieldname4=${data.message}`
-    // window.location = url
-    window.open(url, '_blank', 'width=400,height=600')
-  } catch (error) {
-    console.log(`error opening chat`, error)
-    // dispatch('errorNotification', {title: `Failed to send email`, error})
+// export const openChat = function ({getters, commit, dispatch}, data) {
+//   try {
+//     console.log(`open chat`, data)
+//     // data.name
+//     // data.email
+//     // data.ani
+//     // data.message
+//     // https://ece2.cxdemo.net/system/templates/chat/kiwi/chat.html?subActivity=Chat&entryPointId=1006&templateName=kiwi_alto&languageCode=en&countryCode=US&ver=v11
+//     const template = 'aqua'
+//     const entryPointId = '1002'
+//     const locale = 'en-US'
+//     let url = `https://ece2.cxdemo.net/system/templates/chat/${template}/chat.html`
+//     url += `?subActivity=Chat&entryPointId=${entryPointId}&templateName=${template}&languageCode=${locale.split('-')[0]}&countryCode=${locale.split('-')[1]}&ver=v11`
+//     url += `&fieldname1=${encodeURIComponent(data.name)}&fieldname2=${data.email}&fieldname3=${data.ani}&fieldname4=${data.message}`
+//     // window.location = url
+//     window.open(url, '_blank', 'width=400,height=600')
+//   } catch (error) {
+//     console.log(`error opening chat`, error)
+//     // dispatch('errorNotification', {title: `Failed to send email`, error})
+//   }
+// }
+
+export const openChat = function ({commit, state, getters, rootState, dispatch}, data) {
+  const egainChat = {
+    /* eGain Chat server */
+    serverURL: 'https://ece2.cxdemo.net/system',
+    /* eGain Chat Entry Point */
+    EntryPointId: '1002',
+    /* eGain Chat Locale */
+    Locale: 'en-US',
+    /* eGain template name */
+    Template: 'aqua',
+    /* Set to true to enable posting attributes to templates */
+    PostChatAttributes: true,
+    CustomerContextParameters: {},
+    /* To be called by client website. All the parameters specified in
+    application-chat-defaults must be set here. */
+    SetCustomerParameters: function (egainAttributeName, attributeValue) {
+      egainChat.CustomerContextParameters[egainAttributeName] = attributeValue
+    },
+    /* main chat start function */
+    openHelp: function () {
+      var domainRegex = /^((?:https?:\/\/)?(?:www\.)?([^/]+))/i
+      try {
+        if (egainChat.eglvchathandle !== null && egainChat.eglvchathandle.closed === false) {
+          egainChat.eglvchathandle.focus()
+          return
+        }
+      } catch (err) {
+      }
+      // send form data to fill the form after the window pops open
+      egainChat.SetCustomerParameters('fieldname_1', data.name)
+      egainChat.SetCustomerParameters('fieldname_2', data.email)
+      egainChat.SetCustomerParameters('fieldname_3', data.ani)
+      egainChat.SetCustomerParameters('fieldname_4', data.message)
+      // egainChat.SetCustomerParameters('fieldname_5', true)
+
+      var refererName = ''
+      refererName = encodeURIComponent(refererName)
+      var refererurl = encodeURIComponent(document.location.href)
+      var hashIndex = refererurl.lastIndexOf('#')
+      if (hashIndex !== -1) {
+        refererurl = refererurl.substring(0, hashIndex)
+      }
+      var eglvcaseid = (/eglvcaseid=[0-9]*/gi).exec(window.location.search)
+      var vhtIds = ''
+      if (typeof window.EGAINCLOUD !== 'undefined' && window.EGAINCLOUD.Account.getAllIds) {
+        var ids = window.EGAINCLOUD.Account.getAllIds()
+        vhtIds = '&aId=' + ids.a + '&sId=' + ids.s + '&uId=' + ids.u
+      }
+      var EG_CALL_Q = window.EG_CALL_Q || []
+      EG_CALL_Q.push(['enableTracker', true])
+      var eGainChatUrl = egainChat.serverURL + '/templates/chat/' + egainChat.Template + '/index.html?entryPointId=' + egainChat.EntryPointId
+
+      eGainChatUrl += '' + '&templateName=' + egainChat.Template + '&locale=' + egainChat.Locale + '&ver=v11'
+      eGainChatUrl += '&postChatAttributes=' + egainChat.PostChatAttributes + '&eglvrefname=' + refererName + '&' + eglvcaseid + vhtIds
+
+      var domain = domainRegex.exec(eGainChatUrl)[0]
+
+      if ((eGainChatUrl + refererurl).length <= 2000) {
+        eGainChatUrl += '&referer=' + refererurl
+      }
+      var params = 'height=650,width=450,resizable=yes,scrollbars=yes,toolbar=no'
+
+      if (egainChat.PostChatAttributes) {
+        window.addEventListener('message', function (event) {
+          try {
+            var message = JSON.parse(event.data)
+            if (event.origin === domain && message.Caller.indexOf('EGLV_DOCK') !== -1) {
+              if (message.Key === 'EgainChatParameter') {
+                var response = {
+                  value: egainChat.CustomerContextParameters,
+                  caller: message.Caller
+                }
+                event.source.postMessage(JSON.stringify(response), event.origin)
+              }
+            }
+          } catch (e) {
+          }
+        })
+      }
+      egainChat.eglvchathandle = window.open(eGainChatUrl, '', params)
+    }
   }
+  egainChat.openHelp()
 }
