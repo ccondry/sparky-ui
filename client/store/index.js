be5d1c1d10b7f21182824dd9281e58ddbb01d5b9
Coty Condry <ccondry@cisco.com>
2019-03-27 22:20:07 -0500

    fix websocket address when using development environment

--- client/store/index.js
+++ client/store/index.js
@@ -15,7 +15,13 @@ if (loc.protocol === 'https:') {
 } else {
   wsAddress = 'ws:'
 }
-wsAddress += '//' + loc.host + '/api/v1'
+if (process.env.NODE_ENV === 'production') {
+  // production
+  wsAddress += '//' + loc.host + '/api/v1'
+} else {
+  // dev
+  wsAddress += '//localhost:3020/api/v1'
+}
 
 const store = new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
